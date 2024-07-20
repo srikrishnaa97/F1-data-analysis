024e84e9fa9e9a7a7d8c1914bcea828466e073d6
Srikrishnaa Jayaraman <Srikrishnaa.Jayaraman@blueyonder.com>
2024-07-20 17:10:39 +0530

    results page update

--- basic_functions.py
+++ basic_functions.py
@@ -23,3 +23,72 @@ def rotate(xy, *, angle):
     rot_mat = np.array([[np.cos(angle), np.sin(angle)],
                         [-np.sin(angle), np.cos(angle)]])
     return np.matmul(xy, rot_mat)
+
+def path_to_image_html(path):
+            return '<img src="' + path + '">'
+
+def get_results_df_as_html(data):
+    results = data.results
+    session = data.session_info['Name']
+    if 'Practice' in session:
+        results = pd.merge(right=data.laps.groupby('Driver').LapTime.min().sort_values().reset_index(),
+                            left=data.results, right_on='Driver', left_on='Abbreviation', how='outer')
+        results['Fastest Time'] = results['LapTime']
+        results['LapTime'] = results['LapTime'].apply(convert_timedelta_to_time)
+        results = results.sort_values('LapTime')
+        results['Fastest Time'] = results['Fastest Time'].astype(str)
+        results['Fastest Time'] = results['Fastest Time'].apply(convert_str_date_to_time)
+        results.sort_values('Fastest Time',inplace=True)
+
+
+    results['Headshot'] = results['HeadshotUrl'].map(path_to_image_html)
+    results['TeamColor'] = results['TeamColor'].apply(lambda x: '#' + x)
+
+    results['Time'] = results['Time'].astype(str).apply(convert_str_date_to_time)
+
+    results['Q1'] = results['Q1'].astype(str).apply(convert_str_date_to_time)
+    results['Q2'] = results['Q2'].astype(str).apply(convert_str_date_to_time)
+    results['Q3'] = results['Q3'].astype(str).apply(convert_str_date_to_time)
+
+    cols = ['Headshot', 'Abbreviation', 'FullName', 'TeamName', ]
+    if session == 'Race' or session == 'Sprint':
+        cols = cols + ['ClassifiedPosition', 'GridPosition', 'Time', 'Status', 'Points']
+        results['GridPosition'] = results['GridPosition'].astype(str).map(lambda x: x.split('.')[0])
+    elif session == 'Qualifying' or session == 'Sprint Qualifying' or session == 'Sprint Shootout':
+        cols = cols + ['Q1', 'Q2', 'Q3']
+
+    elif 'Practice' in session:
+        cols.append('Fastest Time')
+
+    else:
+        pass
+
+    html = '<table>'
+    html += '<tr>'
+    for c in cols:
+        html += f'<th>{c}</th>'
+    html += '</tr>'
+    for i in range(len(results)):
+        html += '<tr>'
+        for c in cols:
+            if c == 'TeamName':
+                html += f'<td>{results[c].iloc[i]} <div style="background-color:{results["TeamColor"].iloc[i]};width:4vw;height:1vw;align:center;"></div></td>'
+
+            elif (c == 'Time' and i == 0 and results[c].iloc[i] != 'No Time'):
+                html += f'<td>{results[c].iloc[i][:-3]}</td>'
+
+            elif (c == 'Time' and i > 0 and results[c].iloc[i] != 'No Time'):
+                html += f'<td>+{results[c].iloc[i][3:-3]}</td>'
+
+            elif c.startswith('Q') and results[c].iloc[i] != 'No Time':
+                html += f'<td>{results[c].iloc[i][3:-3]}</td>'
+
+            elif c == 'Fastest Time' and results[c].iloc[i] != 'No Time':
+                html += f'<td>{results[c].iloc[i][3:-3]}</td>'
+
+            else:
+                html += f'<td>{results[c].iloc[i]}</td>'
+        html += '</tr>'
+
+    html += '</table>'
+    return html
--- f1_app.py
+++ f1_app.py
@@ -7,7 +7,7 @@ import datetime as dt
 import pandas as pd
 import numpy as np
 from plot_functions import plot_speed_segments, telemetry, get_driver_laps, lap_times_plot
-from basic_functions import convert_str_date_to_time, convert_timedelta_to_time
+from basic_functions import get_results_df_as_html
 
 st.set_page_config(
     page_title="F1 Data Analysis",
@@ -123,74 +123,8 @@ if display_data_flag:
 
     #       Tab 1
     with tab1:
-        # data = get_session_data()
-        results = data.results
-        if 'Practice' in session:
-            results = pd.merge(right=data.laps.groupby('Driver').LapTime.min().sort_values().reset_index(),
-                               left=data.results, right_on='Driver', left_on='Abbreviation', how='outer')
-            results['Fastest Time'] = results['LapTime']
-            results['LapTime'] = results['LapTime'].apply(convert_timedelta_to_time)
-            results = results.sort_values('LapTime')
-            results['Fastest Time'] = results['Fastest Time'].astype(str)
-            results['Fastest Time'] = results['Fastest Time'].apply(convert_str_date_to_time)
-
         st.header(f'{year} {gp} {session} Results')
-
-
-        def path_to_image_html(path):
-            return '<img src="' + path + '">'
-
-
-        results['Headshot'] = results['HeadshotUrl'].map(path_to_image_html)
-        results['TeamColor'] = results['TeamColor'].apply(lambda x: '#' + x)
-
-        results['Time'] = results['Time'].astype(str).apply(convert_str_date_to_time)
-
-        results['Q1'] = results['Q1'].astype(str).apply(convert_str_date_to_time)
-        results['Q2'] = results['Q2'].astype(str).apply(convert_str_date_to_time)
-        results['Q3'] = results['Q3'].astype(str).apply(convert_str_date_to_time)
-
-        cols = ['Headshot', 'Abbreviation', 'FullName', 'TeamName', ]
-        if session == 'Race' or session == 'Sprint':
-            cols = cols + ['ClassifiedPosition', 'GridPosition', 'Time', 'Status', 'Points']
-            results['GridPosition'] = results['GridPosition'].astype(str).map(lambda x: x.split('.')[0])
-        elif session == 'Qualifying' or session == 'Sprint Qualifying' or session == 'Sprint Shootout':
-            cols = cols + ['Q1', 'Q2', 'Q3']
-
-        elif 'Practice' in session:
-            cols.append('Fastest Time')
-
-        else:
-            pass
-
-        html = '<table>'
-        html += '<tr>'
-        for c in cols:
-            html += f'<th>{c}</th>'
-        html += '</tr>'
-        for i in range(len(results)):
-            html += '<tr>'
-            for c in cols:
-                if c == 'TeamName':
-                    html += f'<td>{results[c].iloc[i]} <div style="background-color:{results["TeamColor"].iloc[i]};width:4vw;height:1vw;align:center;"></div></td>'
-
-                elif (c == 'Time' and i == 0 and results[c].iloc[i] != 'No Time'):
-                    html += f'<td>{results[c].iloc[i][:-3]}</td>'
-
-                elif (c == 'Time' and i > 0 and results[c].iloc[i] != 'No Time'):
-                    html += f'<td>+{results[c].iloc[i][3:-3]}</td>'
-
-                elif c.startswith('Q') and results[c].iloc[i] != 'No Time':
-                    html += f'<td>{results[c].iloc[i][3:-3]}</td>'
-
-                elif c == 'Fastest Time' and results[c].iloc[i] != 'No Time':
-                    html += f'<td>{results[c].iloc[i][3:-3]}</td>'
-
-                else:
-                    html += f'<td>{results[c].iloc[i]}</td>'
-            html += '</tr>'
-
-        html += '</table>'
+        html = get_results_df_as_html(data,session)
         st.markdown(html, unsafe_allow_html=True)
 
     with tab3:
